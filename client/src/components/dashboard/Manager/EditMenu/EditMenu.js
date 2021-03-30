4b1b97ec57a3120d8647e1b292f8095e2214ab83
christopher yu <zyu17@nyit.edu>
2021-03-30 17:52:29 -0400

    editmenu

--- client/src/components/dashboard/Manager/EditMenu/EditMenu.js
+++ client/src/components/dashboard/Manager/EditMenu/EditMenu.js
@@ -1,17 +1,15 @@
-import React, { Component, useState } from 'react';
-import { Link, useHistory} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
-import axios from 'axios';
-import toastr from 'toastr';
+
 import './EditMenu.css';
 
-import MenuApi from './api';
-
 class EditMenu extends Component {
-	constructor() {
-		super();
-		this.state = {
-			menu: {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            menu: {
 				mains: [
 					{
 						Name: 'testing',
@@ -35,74 +33,59 @@ class EditMenu extends Component {
 			error: '',
 			loading: true,
 		};
-	}
-	componentDidMount() {
-		axios
-			.get('http://localhost:5000/Api/Menu/View')
-			.then((res) => {
-				const data = res.data;
-				console.log(data);
-				this.setState({ menu: data });
-			})
-			.catch((err) => this.setState({ error: err }));
-		this.setState({ loading: false });
-	}
-    
-	onFieldChange(event) {
-		const field = event.target.name;
-		let mains = this.state.menu;
-		mains[field] = event.target.value;
-		return this.setState({ mains: mains });
-	}
+	
+        this.handleSubmit = this.handleSubmit.bind(this)
+    };
 
-	onSaveForm(event) {
-		event.preventDefault();
-		MenuApi.CreateMenu(this.state.mains);
-		toastr.success('Successfully created mains item!');
-		this.setState({ mains: {} });
-	}
-	render() {
-       
+    handleChange = (event) => {   
+        const target = event.target;
+        const value = event.target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });     
+    };
 
+    validateForm() {
+        return this.state.itemName.length > 0 && this.state.price.length > 0;
+    };
+
+    handleSubmit = () => {
+        this.props.addItem(this.state, this.props.restaurantId, this.props.locationId);
+        this.props.addItemRow(this.state);
+    };
+
+    render() {
         return (
-        <div className="admin-create-menu-container">
-        <div className="admin-create-menu-content">
-        <h1>Create Menu Item</h1>
+            <div className='admin-create-menu-container'>
+            <div className="admin-create-menu-content">
+            <h1>Create Menu Item</h1>
 
-        <Link to='/Manager/ManagerMenu'>
-          <FiArrowLeft size={16} color="#0c71c3"/>
-          All Menu Items 
-        </Link> 
-        <form onSubmit={this.onSaveForm}>
-          <strong>Name:</strong>
-          <input 
-            type="text"
-            placeholder="Menu Item Name"
-            value={Name}
-            onChange={event => this.setState(event.target.value)}
-          />
-          <strong>Price:</strong>
-          <input 
-            type="number"
-            placeholder="Item Price"
-            value={Price}
-            onChange={event => setPrice(event.target.value)}
-          />
-          <strong>Description:</strong>
-          <input 
-            type="text" 
-            placeholder="Item Description"
-            value={Description}
-            onChange={event => setDescription(event.target.value)}
-          />
+            <Link to='Manager/ManagerMenu'>
+            <FiArrowLeft size={16} color="#0c71c3"/>
+            All Menu Items 
+            </Link> 
+                <input name='itemType' placeholder='Types' value={this.state.itemName} onChange={this.handleChange} />
+                <input name='itemName' placeholder='Name' value={this.state.itemName} onChange={this.handleChange} />
+                <input name='price' placeholder='Price' value={this.state.price} onChange={this.handleChange}/>
+                <input name='description' placeholder='Description' value={this.state.description} onChange={this.handleChange}/>
+                <button onClick={this.handleSubmit} disabled={!this.validateForm()} className='btn_primary' type='submit'>Add Item</button>
+            </div>
+            </div>
+        );
+    };
+};
 
-          <button className="button" type="submit">Create Menu Item</button>
+const mapStateToProps = state => {
+    return {
+        restaurant: state.restaurant
+    };
+};
 
-        </form>
-      </div>
-    </div>
-  );
-}
-}
+function mapDispatchToProps() {
+    return {
+        addItem
+    };
+};
 
-export default EditMenu;
+export default connect(mapStateToProps, mapDispatchToProps())(EditMenu);
