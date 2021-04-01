38a0877e4f132ebda54fcb10daaf9bc39ff44d3b
christopher yu <zyu17@nyit.edu>
2021-03-31 20:19:46 -0400

    booking

--- client/src/components/dashboard/Manager/ViewBooking/ManagerBooking.js
+++ client/src/components/dashboard/Manager/ViewBooking/ManagerBooking.js
@@ -1,49 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { FiEdit } from 'react-icons/fi';
+import { FiEdit FiTrash2 } from 'react-icons/fi';
 import axios from 'axios';
-import './ManagerMenu.css';
-class ManagerMenu extends Component {
+
+
+class ManagerBooking extends Component {
 	constructor() {
 		super();
 		this.state = {
-			menu: {
-				mains: [
-					{
-						Name: 'testing',
-						Description: 'testing',
-						Price: '1',
-					},
-				],
-				sides: [
-					{
-						Name: 'testing',
-						Price: '1',
-					},
-				],
-				drinks: [
-					{
-						Name: 'testing',
-						Price: '1',
-					},
-				],
-			},
-			error: '',
-		};
+			booking: {},
 	}
 	componentDidMount() {
 		axios
-			.get('http://localhost:5000/Api/Menu/View')
+			.get('http://localhost:5000/Api/Reservation/BookingPage')
 			.then((res) => {
 				const data = res.data;
 				console.log(data);
-				this.setState({ menu: data });
+				this.setState({ booking: {} });
 			})
 			.catch((err) => this.setState({ error: err }));
 		this.setState({ loading: false });
 	}
+    
 	render() {
-		const { menu, error, loading } = this.state;
+		const { booking, error, loading } = this.state;
 		return (
 			<div className='admin-view-menu-container'>
 				<div className='admin-view-menu-content'>
@@ -58,22 +38,22 @@ class ManagerMenu extends Component {
   </Link> 
 
   <ul>
-    {bookings.map(booking => (
-      <li key={booking.id}>
+    {booking.map((booking, index) => (
+      <li key={index}>
         <strong>Booking ID:</strong>
-        <p>{booking.id}</p>
-
-        <strong>Slot ID:</strong>
-        <p>{booking.slot_id}</p>
+        <p>{index}</p>
 
         <strong>Date:</strong>
-        <p>{booking.date}</p>
+        <p>{booking.ReservationTime}</p>
 
-        <strong>Date:</strong>
-        <p>{booking.data}</p>
-
-        <strong>People:</strong>
+        <strong>Number of People:</strong>
         <p>{booking.coverNo}</p>
+
+        <strong>First Name:</strong>
+        <p>{booking.FirstName}</p>
+
+        <strong>Last Name:</strong>
+        <p>{booking.lastName}</p>
 
         <button onClick={(event) => handleBookingDeletion(event, booking.id)}>
           <FiTrash2 size={20} color="#a8a8b3"/>
